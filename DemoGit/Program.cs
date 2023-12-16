9d6444d0191d332f65e78d53ee18735550d1a56d
OBJECTIVITY\zmacinska <zmacinska@objectivity.co.uk>
2023-12-16 11:21:34 +0100

    hi all

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -1,5 +1,1 @@
-﻿Console.WriteLine("Hi everyone!");
-Console.WriteLine("I am a programmer!");
-Console.WriteLine("Hello Zuzia");
-Console.WriteLine("Hi Zuzia!");
-Console.WriteLine("Github is awesome");
+﻿Console.WriteLine("Hi all");
